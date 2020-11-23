f01a681e58e77d3f5fe0c8f2d91fc13823899e05
Jochen <webmaster@mapero.de>
2020-11-23 20:47:37 +0100

    chg: Open source obligations from marthoc/docker-deconz

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+# This software includes the marthoc/docker-deconz Docker Image: Copyright (c) 2018 Mark Coombes
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 echo "Starting deCONZ..."
 echo "Current deCONZ version: $DECONZ_VERSION"
@@ -17,12 +36,12 @@ DECONZ_OPTS="--auto-connect=1 \
 if [ "$DECONZ_VNC_MODE" != 0 ]; then
   
   if [ "$DECONZ_VNC_PORT" -lt 5900 ]; then
-    echo "[marthoc/deconz] ERROR - VNC port must be 5900 or greater!"
+    echo "ERROR - VNC port must be 5900 or greater!"
     exit 1
   fi
 
   DECONZ_VNC_DISPLAY=:$(($DECONZ_VNC_PORT - 5900))
-  echo "[marthoc/deconz] VNC port: $DECONZ_VNC_PORT"
+  echo "VNC port: $DECONZ_VNC_PORT"
   
   if [ ! -e /root/.vnc ]; then
     mkdir /root/.vnc
@@ -41,7 +60,7 @@ if [ "$DECONZ_VNC_MODE" != 0 ]; then
   # Export VNC display variable
   export DISPLAY=$DECONZ_VNC_DISPLAY
 else
-  echo "[marthoc/deconz] VNC Disabled"
+  echo "VNC Disabled"
   DECONZ_OPTS="$DECONZ_OPTS -platform minimal"
 fi
 
